8920c98cf1d60026669d538ff8159d0c9b7d8a57
xxy20010606 <xxy20010606@users.noreply.github.com>
2026-06-20 15:13:46 +0800

    📱 添加 PWA 支持：可安装到手机桌面

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = "briefs-v1";
+const ASSETS = [
+  "/briefs/",
+  "/briefs/index.html",
+  "/briefs/finance.html",
+  "/briefs/ai.html",
+  "/briefs/ai-apps.html",
+  "/briefs/manifest.json",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      return (
+        r ||
+        fetch(e.request).then((res) => {
+          if (res.ok) {
+            const clone = res.clone();
+            caches.open(CACHE).then((c) => c.put(e.request, clone));
+          }
+          return res;
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
